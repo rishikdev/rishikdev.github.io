e6c906c7fca89869aee9d1524fc2f4321e47800f
Rishik Dev <82426895+rishikdev@users.noreply.github.com>
2023-01-08 19:53:01 -0600

    UPDATED skills.js

--- Data/skills.js
+++ Data/skills.js
@@ -4,31 +4,51 @@ export default
         card_title : "Programming Languages",
         names : [
             "Java",
-            "Python",
             "C#",
             "JavaScript",
-            "HTML",
+            "Python",
+            "Swift",
+            "HTML5",
             "CSS",
-            "Swift"
+            "SQL"
         ]
     },
 
     {
-        card_title : "Frameworks & Tools",
-        names : [
-            "Bootstrap",
-            "jQuery",
-            "Unity Game Engine",
-            "SwiftUI",
-            "RESTful"
-        ]
-    },
-
-    {
-        card_title : "Databases",
+        card_title : "Frameworks",
         names : [
             "DBMS",
-            "SQL"
+            "RESTful",
+            "MVC",
+            "MVVM",
+            "Spring Boot",
+            "SwiftUI",
+            "AJAX",
+            "CloudKit",
+            "WidgetKit",
+            "Bootstrap",
+        ]
+    },
+
+    {
+        card_title : "Libraries & Tools",
+        names : [
+            "React.js",
+            "jQuery",
+            "Git",
+            "MediaPipe"
+        ]
+    },
+    
+    {
+        card_title : "Software & Services",
+        names : [
+            "Xcode",
+            "Visual Studio",
+            "Visual Studio Code",
+            "AWS",
+            "IntelliJ",
+            "Unity Game Engine"
         ]
     }
 ]
