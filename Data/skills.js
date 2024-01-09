77306d9656a0e685c7db1343544e7c833751af29
RishikDevTerminal <82426895+rishikdev@users.noreply.github.com>
2024-01-09 15:38:45 -0600

    Update Portfolio

--- Data/skills.js
+++ Data/skills.js
@@ -1,52 +1,54 @@
-export default
-[
-    {
-        card_title : "Programming Languages",
-        names : [
-            "Java",
-            "C#",
-            "Swift",
-            "Python",
-            "JavaScript",
-            "HTML",
-            "CSS",
-            "SQL"
-        ]
-    },
+export default [
+  {
+    card_title: "Programming Languages",
+    names: [
+      "Java",
+      "C#",
+      "Swift",
+      "Python",
+      "JavaScript",
+      "TypeScript",
+      "HTML",
+      "CSS",
+      "SQL",
+    ],
+  },
 
-    {
-        card_title : "Frameworks",
-        names : [
-            "SwiftUI",
-            "UIKit",
-            "Core Data",
-            "DBMS",
-            "RESTful",
-            "Bootstrap",
-        ]
-    },
+  {
+    card_title: "Frameworks",
+    names: [
+      "SwiftUI",
+      "UIKit",
+      "Core Data",
+      "DBMS",
+      "RESTful",
+      "Bootstrap",
+      "Tailwind CSS",
+    ],
+  },
 
-    {
-        card_title : "Libraries and Tools",
-        names : [
-            "React.js",
-            "jQuery",
-            "Git",
-            "Swift Package Manager",
-            "CocoaPods"
-        ]
-    },
+  {
+    card_title: "Libraries and Tools",
+    names: [
+      "React.js",
+      "Next.js",
+      "jQuery",
+      "Git",
+      "Swift Package Manager",
+      "CocoaPods",
+    ],
+  },
 
-    {
-        card_title : "Software and Services",
-        names : [
-            "XCode",
-            "Firebase",
-            "Visual Studio",
-            "Visual Studio Code",
-            "IntelliJ",
-            "Unity Game Engine",
-            "MS Office Suite"
-        ]
-    }
-]
+  {
+    card_title: "Software and Services",
+    names: [
+      "XCode",
+      "Firebase",
+      "Visual Studio",
+      "Visual Studio Code",
+      "IntelliJ",
+      "Unity Game Engine",
+      "MS Office Suite",
+    ],
+  },
+];
