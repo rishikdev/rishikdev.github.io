4d4f4d79d76a68556dca050df9eade3dd0c7d239
RishikDevTerminal <82426895+rishikdev@users.noreply.github.com>
2024-12-03 13:08:36 -0600

    Update card-collection.tsx

--- components/custom/card/card-collection.tsx
+++ components/custom/card/card-collection.tsx
@@ -4,7 +4,7 @@ interface CardCollectionProps {
 
 const CardCollection = ({ children }: CardCollectionProps) => {
   return (
-    <div className="grid gap-8 md:grid-cols-2 3xl:grid-cols-3 transition-all duration-500">
+    <div className="grid gap-8 lg:grid-cols-2 3xl:grid-cols-3 transition-all duration-500">
       {children}
     </div>
   );
